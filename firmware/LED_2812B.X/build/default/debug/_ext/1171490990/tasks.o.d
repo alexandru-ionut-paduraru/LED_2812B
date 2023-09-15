81d42f1910471fd239364b6332188e5b1aad75cd
Alexandru Paduraru - Personal <alexandru98@gmail.com>
2023-09-15 15:38:18 +0300

    new file:   firmware/LED_2812B.X/build/default/debug/_ext/1171490990/exceptions.o 	new file:   firmware/LED_2812B.X/build/default/debug/_ext/1171490990/exceptions.o.d 	new file:   firmware/LED_2812B.X/build/default/debug/_ext/1171490990/initialization.o 	new file:   firmware/LED_2812B.X/build/default/debug/_ext/1171490990/initialization.o.d 	new file:   firmware/LED_2812B.X/build/default/debug/_ext/1171490990/interrupts.o 	new file:   firmware/LED_2812B.X/build/default/debug/_ext/1171490990/interrupts.o.d 	new file:   firmware/LED_2812B.X/build/default/debug/_ext/1171490990/tasks.o 	new file:   firmware/LED_2812B.X/build/default/debug/_ext/1171490990/tasks.o.d 	new file:   firmware/LED_2812B.X/build/default/debug/_ext/1360937237/GUI.o 	new file:   firmware/LED_2812B.X/build/default/debug/_ext/1360937237/GUI.o.d 	new file:   firmware/LED_2812B.X/build/default/debug/_ext/1360937237/app.o 	new file:   firmware/LED_2812B.X/build/default/debug/_ext/1360937237/app.o.d 	new file:   firmware/LED_2812B.X/build/default/debug/_ext/1360937237/main.o 	new file:   firmware/LED_2812B.X/build/default/debug/_ext/1360937237/main.o.d 	new file:   firmware/LED_2812B.X/build/default/debug/_ext/163028504/xc32_monitor.o 	new file:   firmware/LED_2812B.X/build/default/debug/_ext/163028504/xc32_monitor.o.d 	new file:   firmware/LED_2812B.X/build/default/debug/_ext/1865161661/plib_dmac.o 	new file:   firmware/LED_2812B.X/build/default/debug/_ext/1865161661/plib_dmac.o.d 	new file:   firmware/LED_2812B.X/build/default/debug/_ext/1865200349/plib_evic.o 	new file:   firmware/LED_2812B.X/build/default/debug/_ext/1865200349/plib_evic.o.d 	new file:   firmware/LED_2812B.X/build/default/debug/_ext/1865254177/plib_gpio.o 	new file:   firmware/LED_2812B.X/build/default/debug/_ext/1865254177/plib_gpio.o.d 	new file:   firmware/LED_2812B.X/build/default/debug/_ext/1865657120/plib_uart6.o 	new file:   firmware/LED_2812B.X/build/default/debug/_ext/1865657120/plib_uart6.o.d 	new file:   firmware/LED_2812B.X/build/default/debug/_ext/298189674/plib_spi1_master.o 	new file:   firmware/LED_2812B.X/build/default/debug/_ext/298189674/plib_spi1_master.o.d 	new file:   firmware/LED_2812B.X/build/default/debug/_ext/60165520/plib_clk.o 	new file:   firmware/LED_2812B.X/build/default/debug/_ext/60165520/plib_clk.o.d 	new file:   firmware/LED_2812B.X/build/default/debug/_ext/60181895/plib_tmr6.o 	new file:   firmware/LED_2812B.X/build/default/debug/_ext/60181895/plib_tmr6.o.d 	new file:   firmware/LED_2812B.X/debug/default/queuelogs/debugtool 	new file:   firmware/LED_2812B.X/dist/default/debug/LED_2812B.X.debug.elf 	new file:   firmware/LED_2812B.X/dist/default/debug/LED_2812B.X.debug.map 	new file:   firmware/LED_2812B.X/dist/default/debug/memoryfile.xml

---
+++ firmware/LED_2812B.X/build/default/debug/_ext/1171490990/tasks.o.d
@@ -0,0 +1,58 @@
+build/default/debug/_ext/1171490990/tasks.o: \
+ ../src/config/default/tasks.c ../src/config/default/configuration.h \
+ ../src/config/default/user.h ../src/config/default/device.h \
+ ../src/config/default/toolchain_specifics.h \
+ ../src/config/default/definitions.h \
+ ../src/config/default/peripheral/tmr/plib_tmr6.h \
+ ../src/config/default/device.h \
+ ../src/config/default/peripheral/tmr/plib_tmr_common.h \
+ ../src/config/default/peripheral/clk/plib_clk.h \
+ ../src/config/default/peripheral/gpio/plib_gpio.h \
+ ../src/config/default/peripheral/evic/plib_evic.h \
+ ../src/config/default/peripheral/dmac/plib_dmac.h \
+ ../src/config/default/peripheral/spi/spi_master/plib_spi1_master.h \
+ ../src/config/default/peripheral/spi/spi_master/plib_spi_master_common.h \
+ ../src/config/default/peripheral/uart/plib_uart6.h \
+ ../src/config/default/peripheral/uart/plib_uart_common.h ../src/app.h \
+ ../src/config/default/configuration.h ../src/GUI.h \
+ ../src/config/default/peripheral/uart/plib_uart6.h
+
+../src/config/default/configuration.h:
+
+../src/config/default/user.h:
+
+../src/config/default/device.h:
+
+../src/config/default/toolchain_specifics.h:
+
+../src/config/default/definitions.h:
+
+../src/config/default/peripheral/tmr/plib_tmr6.h:
+
+../src/config/default/device.h:
+
+../src/config/default/peripheral/tmr/plib_tmr_common.h:
+
+../src/config/default/peripheral/clk/plib_clk.h:
+
+../src/config/default/peripheral/gpio/plib_gpio.h:
+
+../src/config/default/peripheral/evic/plib_evic.h:
+
+../src/config/default/peripheral/dmac/plib_dmac.h:
+
+../src/config/default/peripheral/spi/spi_master/plib_spi1_master.h:
+
+../src/config/default/peripheral/spi/spi_master/plib_spi_master_common.h:
+
+../src/config/default/peripheral/uart/plib_uart6.h:
+
+../src/config/default/peripheral/uart/plib_uart_common.h:
+
+../src/app.h:
+
+../src/config/default/configuration.h:
+
+../src/GUI.h:
+
+../src/config/default/peripheral/uart/plib_uart6.h:
---
+++ firmware/LED_2812B.X/build/default/debug/_ext/1360937237/app.o.d
@@ -0,0 +1,37 @@
+build/default/debug/_ext/1360937237/app.o: ../src/app.c ../src/app.h \
+ ../src/config/default/configuration.h ../src/config/default/user.h \
+ ../src/config/default/device.h \
+ ../src/config/default/toolchain_specifics.h ../src/GUI.h \
+ ../src/config/default/peripheral/uart/plib_uart6.h \
+ ../src/config/default/device.h \
+ ../src/config/default/peripheral/uart/plib_uart_common.h \
+ ../src/config/default/peripheral/tmr/plib_tmr6.h \
+ ../src/config/default/peripheral/tmr/plib_tmr_common.h \
+ ../src/config/default/peripheral/gpio/plib_gpio.h \
+ ../src/config/default/peripheral/dmac/plib_dmac.h
+
+../src/app.h:
+
+../src/config/default/configuration.h:
+
+../src/config/default/user.h:
+
+../src/config/default/device.h:
+
+../src/config/default/toolchain_specifics.h:
+
+../src/GUI.h:
+
+../src/config/default/peripheral/uart/plib_uart6.h:
+
+../src/config/default/device.h:
+
+../src/config/default/peripheral/uart/plib_uart_common.h:
+
+../src/config/default/peripheral/tmr/plib_tmr6.h:
+
+../src/config/default/peripheral/tmr/plib_tmr_common.h:
+
+../src/config/default/peripheral/gpio/plib_gpio.h:
+
+../src/config/default/peripheral/dmac/plib_dmac.h:
